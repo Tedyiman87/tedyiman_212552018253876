e5b7db8b2fdc7283162cf4c3268bb8df02542728
Tedyiman87 <117054222+Tedyiman87@users.noreply.github.com>
2022-10-31 10:39:39 +0700

    Add files via upload

---
+++ MATERI 1 TEDY IMAN-212552018253876.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+    <?php
+        $nama="Tedy Iman";
+        $_123= "text baru";
+
+        echo "<h1>$_123</h1>";
+        echo"Halo ".$nama."<br>";
+        echo"Member";
+    ?>
+</body>
+
+</html>
---
+++ MATERI 2 TEDY IMAN-212552018253876.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Belajar php</title>
+</head>
+
+<body>
+    <?php   
+        echo "<h1>Sekolah Koding</h1>";
+        echo "Halo<br>";
+        echo"Member";
+    ?>
+    Halo Semuanya!
+</body>
+
+</html>
