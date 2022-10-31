ac70e5eec2d2a76f161871a83a8e69a49eedfe0e
Tedyiman87 <117054222+Tedyiman87@users.noreply.github.com>
2022-10-31 10:49:04 +0700

    Add files via upload

---
+++ Materi 7 Loop.php
@@ -0,0 +1,54 @@
+<html>
+  <head>
+    <title>Materi Looping</title>
+  </head>
+
+  <body>
+
+    <?php
+
+    //variable
+    $hewan = ['anjing','kumbang','tokek','laba laba'];
+
+    //for($i=0; $i < count($hewan) -1; $i++)
+      //{
+        //echo "-----------";
+        //echo $hewan [$i];
+        //echo "-----------<br>";
+      //}
+
+
+    //foreach($hewan as $h)
+      //{
+       // echo "-----------";
+       // echo $h;
+        //echo "-----------<br>";
+    //  }
+
+
+  //$data = ['nama' => 'sebastian',
+          //'umur' =>20,
+        //  'sifat'=>'malas'];
+ // foreach($data as $key => $value){
+ //   echo $value. "<br>";
+ // }
+
+
+$i = 5;
+//while($i < count($hewan)){
+ // echo $hewan[$i]. "<br>";
+ // $i++;
+//}
+
+
+do{
+  echo '----------';
+  echo $hewan[$i]. "<br>";
+  $i++;
+}while( $i < count($hewan));
+
+
+
+?>
+  </body>
+</html>
